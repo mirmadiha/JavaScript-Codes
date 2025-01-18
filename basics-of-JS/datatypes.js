453a27f5a9cac9ce4385933a2eea933aa64b7c61
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-01-18 17:51:09 +0530

    basic concepts

---
+++ basics-of-JS/datatypes.js
@@ -0,0 +1,30 @@
+"Use Strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3) 
+ console.log("Coder")
+
+let name="abc";
+let age=18;
+let isLoggedIn=false;
+let address=null;
+let city;
+
+
+//PRIMITIVE DATATYPES
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned yet
+//symbol => uniqueness
+
+
+//OBJECT
+
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ basics-of-JS/variables.js
@@ -0,0 +1,19 @@
+const accountId=12345;
+let email="hello@gmail.com";
+var password="5674";
+accountCity="Sringar";
+let accountState;
+
+// accountId = 2 //not allowed
+
+
+email="me@gmail.com";
+var password="333";
+accountCity="LalChowk";
+
+console.log(password);
+console.table([accountId,email,password,accountCity]);
+/*
+prefernot to use var
+because of issues in block scope and functional scope
+*/
