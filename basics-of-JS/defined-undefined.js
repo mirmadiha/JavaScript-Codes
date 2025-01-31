ca7104cd44b3ed4777dbbf4620e22787e864329c
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-01-31 18:22:19 +0530

    javascript practice

---
+++ basics-of-JS/defined-undefined.js
@@ -0,0 +1,12 @@
+var c;
+console.log(c);
+//console.log(b);
+
+c=10;
+c="hello world";
+if(c===undefined){
+    console.log("c is undefined");
+}
+else{
+    console.log("c is not undefined");
+}
---
+++ basics-of-JS/sum-of-n-natural-numbers.js
@@ -0,0 +1,8 @@
+let sum = 0;
+let n= 10;
+
+for(let j=1;j<=n;j++){
+    sum+=j;
+}
+
+console.log("sum = ",sum);
