1922d8566897a47f4c99fd0818611d335b1e546d
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-02-06 23:57:51 +0530

    functions in javascript

--- functions/sum-of-two-numbers.js
+++ functions/sum-of-two-numbers.js
@@ -2,5 +2,12 @@ function sumOfTwoNumbers(a,b){
     return a+b;
 }
 
+
+//Arrow function
+const arrowProduct=(a,b)=>{
+    console.log(a*b);
+}
+
+arrowProduct(10,5);
 let result=sumOfTwoNumbers(10,20);
 console.log(`The sum of two numbers is ${result}`);
