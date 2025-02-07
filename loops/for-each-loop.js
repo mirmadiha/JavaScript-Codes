06c5f534af0d7cf1a1bde25b43df46fcbab8ee6b
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-02-07 23:49:33 +0530

    for Each loop  in javascript

--- loops/for-each-loop.js
+++ loops/for-each-loop.js
@@ -2,9 +2,6 @@
 it takes a callback function as an argument and runs that callback function on each
 element of the array. 
 
-The callback function takes three arguments: 
-the value of the element, the index of the element, and the array itself. 
-
 The forEach loop does not return anything. It is used to perform some operation on 
 each element of the array.   
 */
@@ -12,3 +9,13 @@ let arr=[1,2,3,4,5];
 arr.forEach((val)=>{
     console.log(val);
 })
+
+/*The callback function takes three arguments: 
+the value of the element, 
+the index of the element, 
+and the array itself.  */
+
+let cities=["Delhi","Mumbai","Chennai","Kolkata"];
+cities.forEach((val,index,cities)=>{
+    console.log(`${val} is at index ${index} in the array ${cities}`);
+})  
