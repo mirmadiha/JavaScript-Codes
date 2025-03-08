abd90d05e465a0617064785db891b8e2459a26fb
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-03-08 23:45:06 +0530

    practice

--- DOM/script.js
+++ DOM/script.js
@@ -5,8 +5,10 @@ Access them and add some unique text to each of them
 
 let boxes=document.querySelectorAll(".box");
 console.log(boxes);
-boxes[0].innerText="I am box 1";
-boxes[1].innerText="I am box 2";
-boxes[2].innerText="I am box 3";
+let i =0;
+for(div of boxes){
+    boxes[i].innerHTML="I am box "+ i;
+    i++;
+}
 
 
