49a1151b26d6f7c4dd6fe2df2bc4fc69c8cf2066
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-03-09 12:24:47 +0530

    DOM Manipulation

--- DOM/script.js
+++ DOM/script.js
@@ -1,12 +1,7 @@
-//Attributes
+//style
 
-////getAttribute() method
-let attr=document.querySelector("div");
-console.log(attr);
-console.log(attr.getAttribute("class")); 
+////node style
+let div=document.querySelector("div");
+console.log(div.style);
 
-
-////setAttribute() method
-attr.setAttribute("name","JAVA");
-console.log(attr.getAttribute("name"));
 
