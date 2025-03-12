7baa12547a9c29e50b6f90201fd154f4847095ac
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-03-12 23:18:37 +0530

    Event Handling

--- DOM/script.js
+++ DOM/script.js
@@ -1,4 +1,8 @@
 let div=document.querySelector("div");
 div.onmouseover=function(e){
     console.log(e);
+    console.log(e.type);
+    console.log(e.target);
+    console.log(e.clientX);
+    console.log(e.ClientY);
 }
