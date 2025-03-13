d3e1873e453993701265f19b4d78a7d3f5c5fd7a
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-03-13 23:45:34 +0530

    Event Handling

--- DOM/script.js
+++ DOM/script.js
@@ -1,8 +1,5 @@
+
 let div=document.querySelector("div");
-div.onmouseover=function(e){
-    console.log(e);
-    console.log(e.type);
-    console.log(e.target);
-    console.log(e.clientX);
-    console.log(e.ClientY);
-}
+div.addEventListener("click",function(){
+    alert("Hello World");
+})
