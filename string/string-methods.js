e268ca974cbc93e941d6dd451d8f4a6905b05531
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-05-01 00:03:01 +0530

    string methods

--- string/string-methods.js
+++ string/string-methods.js
@@ -6,3 +6,7 @@ console.log(name.toUpperCase()); //convert to upper case
 console.log(name.toLowerCase());//convert to lower case
 
 console.log(name.slice(2,4)); //slice the string from index 2 to 4
+
+console.log(name.slice(2)); //slice the string from index 2 to end
+
+console.log(name.replace("Har","Mar")); //replace the string "Har" with "Mar"
