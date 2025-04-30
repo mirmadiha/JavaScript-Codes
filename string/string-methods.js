8b9b6ba457863c7d397a3b345038de2b74630e5f
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-04-30 23:59:08 +0530

    string methods

--- string/string-methods.js
+++ string/string-methods.js
@@ -4,3 +4,5 @@ console.log(name.length); //length of string
 console.log(name.toUpperCase()); //convert to upper case
 
 console.log(name.toLowerCase());//convert to lower case
+
+console.log(name.slice(2,4)); //slice the string from index 2 to 4
