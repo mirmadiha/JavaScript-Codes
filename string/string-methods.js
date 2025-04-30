add969b29062c6002152b9caf5c94d30ea31bc4f
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-05-01 00:07:41 +0530

    string methods

--- string/string-methods.js
+++ string/string-methods.js
@@ -10,3 +10,11 @@ console.log(name.slice(2,4)); //slice the string from index 2 to 4
 console.log(name.slice(2)); //slice the string from index 2 to end
 
 console.log(name.replace("Har","Mar")); //replace the string "Har" with "Mar"
+
+let friend="Potter";
+console.log(friend.concat(" is a friend of mine"));//concatenate the string with another string
+
+let friend2="   Hermione   ";
+console.log(friend2);//print the string with spaces
+
+console.log(friend2.trim());//remove the spaces from the string
