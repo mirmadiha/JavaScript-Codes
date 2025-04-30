2dfa51130692d0147017409cb37a4eef5325e6ff
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-04-30 23:57:44 +0530

    string methods

--- string/string-methods.js
+++ string/string-methods.js
@@ -1,2 +1,6 @@
 let name="Harry";
 console.log(name.length); //length of string
+
+console.log(name.toUpperCase()); //convert to upper case
+
+console.log(name.toLowerCase());//convert to lower case
