41c478a3e88f03cc95b302d05763c2a3e8becbc6
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-05-10 19:48:30 +0530

    set timeout

--- DOM/script3.js
+++ DOM/script3.js
@@ -1,3 +1,9 @@
-first.className="yellow text";
-first.classList.remove("yellow");
-first.classList.add("red");
+// first.className="yellow text";
+// first.classList.remove("yellow");
+// first.classList.add("red");
+
+document.write("hello world!");
+
+setTimeout(function(){
+    alert("Hey this is an alert!");
+},2000);
