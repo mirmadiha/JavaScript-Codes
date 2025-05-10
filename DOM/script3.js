885227a8911d5d529de526c8064a80e3f2113d11
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-05-10 19:58:42 +0530

    set timeout

--- DOM/script3.js
+++ DOM/script3.js
@@ -4,6 +4,13 @@
 
 document.write("hello world!");
 
-setTimeout(function(){
+let a=setTimeout(function(){
     alert("Hey this is an alert!");
 },2000);
+
+const answer=prompt("Do you want to run set timeout? Yes or No?");
+
+if("No"==answer || "NO"==answer || "no"==answer ){
+clearTimeout(a); // This will cancel the timeout set above
+}
+console.log(a);
