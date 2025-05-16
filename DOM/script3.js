dd7495b74427201e29d039563028b662ca08632f
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-05-16 23:58:35 +0530

    set interval

--- DOM/script3.js
+++ DOM/script3.js
@@ -2,15 +2,30 @@
 // first.classList.remove("yellow");
 // first.classList.add("red");
 
-document.write("hello world!");
+// document.write("hello world!");
 
-let a=setTimeout(function(){
-    alert("Hey this is an alert!");
-},2000);
+// let a=setTimeout(function(){
+//     alert("Hey this is an alert!");
+// },2000);
 
-const answer=prompt("Do you want to run set timeout? Yes or No?");
+// const answer=prompt("Do you want to run set timeout? Yes or No?");
 
-if("No"==answer || "NO"==answer || "no"==answer ){
-clearTimeout(a); // This will cancel the timeout set above
+// if("No"==answer || "NO"==answer || "no"==answer ){
+// clearTimeout(a); // This will cancel the timeout set above
+// }
+// console.log(a);
+
+const sum=(a,b)=>{
+    console.log("The sum function is running!"+(a+b));
+    a+b;
 }
-console.log(a);
+
+setTimeout(sum,2000,6,4);
+// The above line will run the sum function after 2 seconds with 6 and 4 as arguments
+
+const Greeting=()=>{
+    alert("Hello, this is a greeting function!");
+}
+setInterval(Greeting,2000);
+// The above line will run the Greeting function every 2 seconds
+
