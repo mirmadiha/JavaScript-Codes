e9a4f6c8e68a9096d7c4aea02e98daa474821039
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-06-01 20:53:28 +0530

    inbuilt objects in JS

---
+++ objects/inbuilt-objects.js
@@ -0,0 +1,18 @@
+//there are inbuilt objects in javascript like :
+// Maths and Date
+
+console.log("-----------MATH----------");
+
+console.log("square root of 4 is: ",Math.sqrt(4));
+console.log("square of 4 is: ",Math.pow(4,2));
+console.log("Find maximum number: ",Math.max(23,45,66,77,443));
+console.log("Find minimum number: ",Math.min(23,45,66,77,443));
+console.log("Find the nearest greatest integer of 2.4: ",Math.ceil(2.4));
+console.log("Find the nearest smallest integer of 2.4: ",Math.floor(2.4));
+console.log("Find the round off of 2.5: ",Math.round(2.5));
+console.log("The value of PI is: ",Math.PI);
+
+
+console.log("-----------DATE----------");
+const currentDate=new Date();
+console.log("CurrentDate:",currentDate);
