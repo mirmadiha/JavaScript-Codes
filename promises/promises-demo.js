a6c7b9c81b9d9c4d9fc1e4c21cd26d5636b90fbe
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-06-05 16:35:30 +0530

    Promises in javascript

--- promises/promises-demo.js
+++ promises/promises-demo.js
@@ -4,24 +4,57 @@
 
 //CALLBACKS->CALLBACK HELL
 
-let myPromise=new Promise((resolve,reject)=>{
-  let a=Math.random();
+// let myPromise=new Promise((resolve,reject)=>{
+//   let a=Math.random();
 
-  if(a<0.5){
-    resolve("The number is < 0.5");
-  }
-  else{
-    reject("The number > 0.5");
-  }
-});
-myPromise.then(
-  function(message){
-    console.log( message); 
-    return "forward message";
-  }
-).then(
-  function(message){
-    console.log( message); 
-  }
-  ).catch((message)=>console.error(message));
+//   if(a<0.5){
+//     resolve("The number is < 0.5");
+//   }
+//   else{
+//     reject("The number > 0.5");
+//   }
+// });
+// myPromise.then(
+//   function(message){
+//     console.log( message); 
+//     return "forward message";
+//   }
+// ).then(
+//   function(message){
+//     console.log( message); 
+//   }
+//   ).catch((message)=>console.error(message));
+
+
+// let myPromise=new Promise((resolve,reject)=>{
+//   setTimeout(()=>console.log("hey"),0);
+//   resolve("The timer is of 5 seconds");
+// });
+// myPromise
+// .then((message)=>console.log(message))
+// .catch((err)=>console.log(err));
+
+//promise.All()
+let promise1=new Promise((resolve,reject)=>{
+    setTimeout(()=>console.log("promise1"),3000);
+    resolve("Promise1 fulfilled");
+})
+
+let promise2=new Promise((resolve,reject)=>{
+    setTimeout(()=>console.log("promise2"),5000);
+    reject("Promise2 rejected");
+})
+
+let promise3=new Promise((resolve,reject)=>{
+    setTimeout(()=>console.log("promise3"),3000);
+    resolve("Promise3 fulfilled");
+})
+
+Promise.all([promise1,promise2,promise3])
+.then((values)=>console.log(values))
+.catch((error)=>console.error(error));
+
+
+
+
 
